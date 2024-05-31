eff5e6498e7348241e2d0f0db318dccbca628dc4
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-05-31 16:19:44 +0300

    Navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import styled, {ThemeProvider} from "styled-components";
+import {darkTheme} from "./utils/Themes";
+import Navbar from "./components/Navbar"
+
+const Body = styled.div`
+  background-color: ${({theme}) => theme.bg};
+  width:100%;
+  height: 100%;
+  overflow-x:hidden;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      Portfolio
-    </div>
+    <ThemeProvider theme={darkTheme}>
+       <Navbar />
+      <Body>
+       
+      </Body>
+    </ThemeProvider>
   );
 }
 
